803945077a3fc96b5db9739a8345a5711fc621da
wkrud <wkrud@DESKTOP-94FPN0J>
2020-01-04 15:37:24 +0900

    2020-01-04 이새힘&박지민 테스트 ㅎㅎ

--- src/com/lec/beans/MbDAO.java
+++ src/com/lec/beans/MbDAO.java
@@ -1,5 +1,39 @@
 package com.lec.beans;
 
-public class MbDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import common.D;
+
+public class MbDAO {
+	Connection conn;
+	PreparedStatement pstmt;
+	Statement stmt;
+	ResultSet rs;
+	
+	// DAO 객체가 생성될때 Connection 도 생성된다!
+	public MbDAO() {
+		
+		try {
+			Class.forName(D.DRIVER);
+			conn = DriverManager.getConnection(D.URL, D.USERID, D.USERPW);
+			System.out.println("WriteDAO 객체 생성, 데이터베이스 연결");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// DB 자원 반납 메소드
+	public void close() throws SQLException {
+		if(rs != null) rs.close();
+		if(pstmt != null) pstmt.close();
+		if(stmt != null) stmt.close();
+		if(conn != null) conn.close();
+	}
 }
