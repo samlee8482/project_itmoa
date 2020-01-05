9018d606401513911a6188a06b38e8876457fd1d
wkrud <wkrud@DESKTOP-94FPN0J>
2020-01-05 18:27:05 +0900

    2020-01-05 test

---
+++ src/com/lec/beans/NewsDAO.java
@@ -0,0 +1,109 @@
+package com.lec.beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import common.D;
+
+public class NewsDAO {
+	
+	Connection conn;
+	PreparedStatement pstmt;
+	Statement stmt;
+	ResultSet rs;
+	
+	public NewsDAO() {
+		try {
+			Class.forName(D.DRIVER);
+			conn = DriverManager.getConnection(D.URL, D.USERID, D.USERPW);
+			System.out.println("ReviewDAO() 객체 생성, 데이터베이스 연결");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// DB 자원 반납 메소드
+	public void close() throws SQLException {
+		if(rs != null) rs.close();
+		if(pstmt != null) pstmt.close();
+		if(stmt != null) stmt.close();
+		if(conn != null) conn.close();
+	}
+	
+	public NewsDTO [] createReviewArray(ResultSet rs) throws SQLException {
+		
+		ArrayList<NewsDTO> newsList = new ArrayList<NewsDTO>();
+		
+		while(rs.next()){
+
+			int review_brd_uid = rs.getInt("review_brd_uid");
+			String mb_id = rs.getString("mb_id");
+			String ins_name = rs.getString("ins_name");
+			String review_brd_regdate = rs.getString("review_brd_regdate");
+			String review_brd_title = rs.getString("review_brd_title");
+			int review_brd_viewcnt = rs.getInt("review_brd_viewcnt");
+			
+			ReviewDTO dto = new ReviewDTO(review_brd_uid, mb_id, ins_name, review_brd_regdate, review_brd_title, review_brd_viewcnt);
+			reviewList.add(dto);			
+		}
+		
+		int size = reviewList.size();
+		ReviewDTO [] arr = new ReviewDTO[size];
+		
+		reviewList.toArray(arr);
+		
+		return arr;
+	}
+	
+	// 2.
+	public ReviewDTO[] selectReviewList(int option_review, String keyword) throws SQLException {
+		
+		
+		ReviewDTO [] arr = null;
+		String selectReview = D.SQL_SELECT_REVIEW;
+		
+		// 후기 검색 조건 (1)회원ID  (2)후기제목   (3)후기내용
+		
+		switch(option_review) {
+			case 1: 
+				selectReview += D.SQL_SELECT_REVIEW_BRD_WHERE_USER_ID;
+				break;
+			case 2:
+				selectReview += D.SQL_SELECT_REVIEW_BRD_WHERE_REVIEW_TITLE;
+				break;
+			case 3:
+				selectReview += D.SQL_SELECT_REVIEW_BRD_WHERE_REVIEW_CONTENT;
+				break;
+			default:
+				break;
+		}
+		
+		// 정렬
+		selectReview += D.SQL_ORDER_REVIEW;
+		
+		try {
+			// keyword가 있을 경우 쿼리문에 키워드 넘겨주기
+			if(keyword != null && !keyword.equals("")) {
+				pstmt = conn.prepareStatement(selectReview);
+				pstmt.setString(1, keyword);
+			}else { 
+				pstmt = conn.prepareStatement(selectReview);
+			}
+
+			rs = pstmt.executeQuery();
+			arr = createReviewArray(rs);
+		} finally {
+			close();
+		}		
+		
+		return arr;
+	}
+	
+}
