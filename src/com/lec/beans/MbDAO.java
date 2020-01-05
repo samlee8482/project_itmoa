5f9cb2b9d559303eb3cbe7159faeb2f6c94d0a3d
wkrud <wkrud@DESKTOP-94FPN0J>
2020-01-05 12:10:30 +0900

    2020-01-04 MbDAO 로그인페이지에 관한 고찰..

--- src/com/lec/beans/MbDAO.java
+++ src/com/lec/beans/MbDAO.java
@@ -22,7 +22,7 @@ public class MbDAO {
 		try {
 			Class.forName(D.DRIVER);
 			conn = DriverManager.getConnection(D.URL, D.USERID, D.USERPW);
-			System.out.println("WriteDAO 객체 생성, 데이터베이스 연결");
+			System.out.println("MbDAO 객체 생성, 데이터베이스 연결");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -63,7 +63,7 @@ public class MbDAO {
 		MbDTO[] arr = null;
 		
 		try {
-			pstmt = conn.prepareStatement(D.SQL_SELECT_LOGIN);
+			pstmt = conn.prepareStatement(D.SQL_SELECT_LOGIN);	// 검증 어떤식으로? 이미지는 마이페이지에서, 레벨은 게시글 쓸떄필요한게 아닌가?
 			pstmt.setString(1, mb_id);
 			pstmt.setString(2, mb_pw);
 			rs = pstmt.executeQuery();
@@ -73,6 +73,39 @@ public class MbDAO {
 		}
 		
 		return arr; 
+	}
+	
+	public int loginCheck(String mb_id, String mb_pw) {
+		MbDTO[] arr = null;
+		
+		String dbPW = "";	// db 에서 꺼낸 비밀번호 담을 변수
+		int x = -1;
+		
+		//String SQL_SELECT_MB_PW_BY_ID = "SELECT mb_uid, mb_id, mb_pw, mb_level, mb_img FROM mb WHERE ID = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(D.SQL_SELECT_LOGIN);
+			pstmt.setString(1, mb_id);
+			pstmt.setString(2, mb_pw);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) // 입려된 아이디에 해당하는 비번 있을경우
+            {
+                dbPW = rs.getString("mb_pw"); // 비번을 변수에 넣는다.
+ 
+                if (dbPW.equals(mb_pw)) 
+                    x = 1; // 넘겨받은 비번과 꺼내온 배번 비교. 같으면 인증성공
+                else                  
+                    x = 0; // DB의 비밀번호와 입력받은 비밀번호 다름, 인증실패
+                
+            } else {
+                x = -1; // 해당 아이디가 없을 경우
+            }
+ 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return x;
 	}
 	
 	// 회원가입
@@ -90,7 +123,6 @@ public class MbDAO {
 			pstmt.setString(7, mb_add2);
 			cnt = pstmt.executeUpdate();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 		
@@ -98,6 +130,7 @@ public class MbDAO {
 	}
 	
 	// 마이페이지
+	// 1.
 	public MbDTO[] createMypageArr(ResultSet rs) throws SQLException {	// 데이터 우리가 쓸 수 있는 값으로 바꿔오기
 		ArrayList<MbDTO> list = new ArrayList<MbDTO>();
 
@@ -124,6 +157,7 @@ public class MbDAO {
 		return arr;
 	}
 	
+	// 2.
 	public MbDTO[] myPage(int mb_uid) {
 		MbDTO[] arr = null;
 		
@@ -139,6 +173,7 @@ public class MbDAO {
 		return arr; 
 	}
 	
+	// 회원정보 수정
 	public int update(String mb_pw, String mb_email, int mb_zip, String mb_add1, String mb_add2, int mb_uid) throws SQLException{
 		int cnt = 0;
 		
