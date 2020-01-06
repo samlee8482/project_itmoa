ab35e3cc547b020a783950eaba8d7ddaa756d175
wkrud <wkrud@DESKTOP-94FPN0J>
2020-01-06 18:24:33 +0900

    충돌

--- src/com/lec/beans/AdminClassDAO.java
+++ src/com/lec/beans/AdminClassDAO.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+         
 import common.D;
 
 public class AdminClassDAO {
