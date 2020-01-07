69574b6e4cab0f67754d75b018dd412b58852fa7
narij <narij@DESKTOP-KH2FN7U>
2020-01-07 17:10:51 +0900

    2020-01-07 AdminClassDAO insView,createInsArray 메소드 추가 ClassDTO 생성자 추가 AdmininsViewCommand 작성 - 박우람

--- src/command/AdminInsViewCommand.java
+++ src/command/AdminInsViewCommand.java
@@ -1,14 +1,30 @@
 package command;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.lec.beans.AdminClassDAO;
+import com.lec.beans.ClassDTO;
 
 public class AdminInsViewCommand implements Command {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
+		
+		ClassDTO [] insArr = null; 
+		AdminClassDAO dao = new AdminClassDAO();
 
-	}
+		try {
+			insArr = dao.insView();
+			if (insArr.length == 1) {
+				request.setAttribute("adminInsView", insArr);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}	
 
 }
