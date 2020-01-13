716f58f2c045db91aa50361e216cd6757ea9e8d6
Miji <Miji@DESKTOP-0RQ0LCF>
2020-01-13 19:41:43 +0900

    ㄴㅇㄹㄴㄹㅇㄹㅇㄴ

--- src/command/AdminMemberListCommand.java
+++ src/command/AdminMemberListCommand.java
@@ -13,15 +13,13 @@ public class AdminMemberListCommand implements Command {
 	
    @Override
    public void execute(HttpServletRequest request, HttpServletResponse response) {
-	  
-	   	System.out.println(request.getParameter("option_mb_1"));
-		System.out.println(request.getParameter("option_mb_2"));
-		System.out.println(request.getParameter("option_mb_3"));
+	  	
 		String str_option_mb_1 = request.getParameter("option_mb_1");
 		String str_option_mb_2 = request.getParameter("option_mb_2");
 		String option_mb_3 = request.getParameter("option_mb_3");
 		int option_mb_1 = 0;
 		int option_mb_2 = 0;
+		
 		if (str_option_mb_1 == null || str_option_mb_2 == null) {
 			option_mb_1 = 1;
 			option_mb_2 = 5;
@@ -94,40 +92,7 @@ public class AdminMemberListCommand implements Command {
 		}
 	  
 		
-//      System.out.println(request.getParameter("option_mb_1"));
-//      System.out.println(request.getParameter("option_mb_2"));
-//      System.out.println(request.getParameter("option_mb_3"));
-//      String str_option_mb_1 = request.getParameter("option_mb_1");
-//      String str_option_mb_2 = request.getParameter("option_mb_2");
-//      String option_mb_3 = request.getParameter("option_mb_3");
-//      int option_mb_1 = 0;
-//      int option_mb_2 = 0;
-//      if (str_option_mb_1 == null || str_option_mb_2 == null) {
-//         option_mb_1 = 1;
-//         option_mb_2 = 5;
-//         option_mb_3 = "a";
-//      } else {
-//         option_mb_1 = Integer.parseInt(request.getParameter("option_mb_1"));
-//         option_mb_2 = Integer.parseInt(request.getParameter("option_mb_2"));
-//      }
-//      AdminMbDAO dao = new AdminMbDAO();
-//      MbDTO [] arr = null;
-//      
-//      System.out.println(option_mb_1 + "," + option_mb_2 + "," + option_mb_3);
-//      
-//      if (option_mb_1 > 0 
-//      && option_mb_2 > 0 
-//      && option_mb_3.length() > 0 && !option_mb_3.trim().equals("") && option_mb_3 != null) {
-//         try {
-//            arr = dao.selectMb(option_mb_1, option_mb_2, option_mb_3);
-//            request.setAttribute("adminMemberList", arr);
-//         } catch (SQLException e) {
-//            e.printStackTrace();
-//         } catch (NamingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//      }
+
    }
 
 }
